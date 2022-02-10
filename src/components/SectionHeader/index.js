5b5a76d6752b7b3135488b02cbbda740a71a8efa
mdeamf <mauriciodealmeidamf@gmail.com>
2022-02-10 17:40:07 -0300

    feat: InfoSection content

--- src/components/SectionHeader/index.js
+++ src/components/SectionHeader/index.js
@@ -1,12 +1,12 @@
 import './styles.scss';
 
-const SectionHeader = ({ title }) => {
+const SectionHeader = ({ title, invert }) => {
   return (
     <div className="section-header">
       <h1 className="display-5">
         <span>
           {title}
-          <span className="underscore">_</span>
+          <span className={`underscore ${invert ? 'invert' : ''}`}>_</span>
         </span>
       </h1>
     </div>
